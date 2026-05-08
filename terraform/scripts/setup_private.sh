3c7b04551997d533043fbf2a9d1ab3815b67f6f0
RobertMarton04 <151923121+RobertMarton04@users.noreply.github.com>
2026-05-08 11:41:08 +0200

    Update setup_private.sh
    Comienzo del script

---
+++ terraform/scripts/setup_private.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+dnf update -y
+dnf install -y docker
+systemctl enable --now docker
+mkdir -p /home/ec2-user/nextcloud && cd /home/ec2-user/nextcloud
+
+cat <<EOF > docker-compose.yml
+services:
+  db:
+    image: postgres:15
+    environment:
+      - POSTGRES_PASSWORD=nc_pass
+      - POSTGRES_DB=nextcloud
+      - POSTGRES_USER=nextcloud
+  app:
+    image: nextcloud:latest
+    ports: ["80:80"]
+    environment:
+      - POSTGRES_HOST=db
+      - POSTGRES_DB=nextcloud
+      - POSTGRES_USER=nextcloud
+      - POSTGRES_PASSWORD=nc_pass
+      - NEXTCLOUD_TRUSTED_DOMAINS=*
+EOF
+docker compose up -d
